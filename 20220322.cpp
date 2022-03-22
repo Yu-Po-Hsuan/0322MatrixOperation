3af92a6fcef2a1a8c0fe5eb2cf416bbe0d46a187
yupohsuan <yph3542@gmail.com>
2022-03-22 15:46:18 +0800

    矩陣乘法

---
+++ 20220322.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#define m 3
+using namespace std;
+int main(){
+	int a[m][m]={
+		{1,0,0},
+		{0,1,0},
+		{0,0,1}
+	};
+	int b[m][m]={
+		{1,2,3},
+		{4,5,6},
+		{7,8,9}
+	};
+	
+	int c[m][m];
+	
+	for(int i=0;i<m;i++){
+		for(int j=0;j<m;j++){
+			int sum=0;
+			for(int k=0;k<m;k++){
+				sum+=a[i][k]*b[k][j];
+			}
+			c[i][j]=sum;
+		}
+	}
+
+	for(int i=0;i<m;i++){
+		for(int j=0;j<m;j++){
+			cout<<c[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+		
+			
+}
+
